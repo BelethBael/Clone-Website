3e162a7280bc71096d92bf9d8e4c8d2b6ed7f82d
Ananda Maytha <59648862+BelethBael@users.noreply.github.com>
2022-05-12 20:46:27 +0700

    Test_Update

---
+++ twitter-clone/src/page/login/login.js
@@ -0,0 +1,31 @@
+import React from "react";
+import '../css-configs/Login.css'
+
+class Login extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
+    render(){
+        console.log("test");
+        return(
+            <div className="login-main">
+                <div className="login-up">
+
+                </div>
+
+                <div className="login-mid">
+
+                </div>
+                <div className="login-bottom">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
